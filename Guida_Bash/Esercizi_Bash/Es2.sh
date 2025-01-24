66f6941bdf88b4311c88179647eddca141356c5a
TeoIce <matteo.saramin@istitutovolterra.edu.it>
2025-01-24 22:08:11 +0100

    Esercizio 2

--- Guida_Bash/Esercizi_Bash/Es2.sh
+++ Guida_Bash/Esercizi_Bash/Es2.sh
@@ -1,2 +1,47 @@
 #!/bin/bash
-echo "cia"
+#Programma con l'uso di costrutti condizionali e operazioni aritmetiche
+
+while true; do      #Tramite l'uso del ciclo interrativo "while", è possibile usare più volte la calcolatrice
+
+    echo "Scrivi il primo valore"
+    read a
+    echo "Scrivi il secondo valore"
+    read b
+    echo "Scrivi l'operazione che vuoi svolgere tra queste: "+", "-", "*", "/", "**", "%"."
+    read operazione
+
+    case $operazione in
+        "+")
+            echo "Addizione"
+            echo "Risultato: $((a+b))";;
+        "-")
+            echo "Sottrazione"
+            echo "Risultato: $((a-b))";;
+        "*")
+            echo "Moltiplicazione"
+            echo "Risultato: $((a*b))";;
+        "/")
+            echo "Divisione"
+            if [[ $b -eq 0 ]]; then
+            echo "Errore: Valore di b non tollerato"
+            else 
+            echo "Risultato: $((a/b))"
+            fi;;
+        "**")
+            echo "Esponenziale"
+            echo "Risultato: $((a**b))";;
+        "%")
+            echo "Resto"
+            echo "Risultato: $((a%b))";;
+        *)
+            echo "Errore: Operazione non disponibile";;
+    esac
+
+    echo "Vuoi fare un'altra operazione? [y/n]"         #Uso del "if" per uscire dal programma
+    read risposta
+    if [[ $risposta -eq n ]]; then 
+        echo "Uscita dal programma."
+        break
+    fi
+
+done
