2db318fab235ff3fcd9c938c4c887904395b6ed3
TeoIce <matteo.saramin@istitutovolterra.edu.it>
2025-02-07 18:47:44 +0100

    Esercizio di Lettura Input

---
+++ Guida_Bash/Esercizi_Bash/Es_Input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#In questo esercizio vedremo il funzionamento del comando "read", con le eventuali opzioni anche combinate.
+
+read -r -p "Lettura di a, con messaggio di richiesta; per comodità usero l'opzione -p anche nei prossimi esempi: " a
+echo "a = " "${a}"
+
+read -r -p "Lettura di b, limitando il numero di caratteri da inserire a 5: " -n5 b
+echo ""                                                                 #Questo "echo" è necessario per mandare accapo l'echo succesivo, dato che non utilizziamo enter.
+echo "b = " "${b}"
+
+read -r -p "Lettura di c, nascondendo l'input dal terminale" -s c
+echo ""
+echo "c = " "${c}"
